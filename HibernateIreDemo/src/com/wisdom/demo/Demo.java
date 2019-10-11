cdc50c995646ef4871b04d665b4d3729b7042cce
IreNadee <nadeeshaire@gmail.com>
2019-10-11 10:12:02 +0530

    First commit

---
+++ HibernateIreDemo/src/com/wisdom/demo/Demo.java
@@ -0,0 +1,31 @@
+package com.wisdom.demo;
+
+import com.wisdom.dao.EmployeeManager;
+import com.wisdom.model.Employee;
+
+public class Demo {
+	EmployeeManager manager = null;
+
+	public Demo() {
+		manager = new EmployeeManager();
+	}
+
+	public void saveData() {
+		Employee employee = new Employee();
+		employee.setId(100);
+		employee.setFirstName("Asela");
+		employee.setLastName("Liyanage");
+		employee.setSalary(10000);
+
+		boolean saveState = manager.saveEmployeeData(employee);
+		System.out.println("--saveState-->" + saveState);
+
+	}
+
+	public static void main(String[] args) {
+
+		Demo demo = new Demo();
+		demo.saveData();
+	}
+
+}
